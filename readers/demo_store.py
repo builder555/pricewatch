43c099b95bee2c50cad496b0f5b4edc5615e70cd
Robert'); DROP TABLE students; -- <85308587+builder555@users.noreply.github.com>
2023-10-21 21:07:47 -0400

    example price reader added

---
+++ readers/demo_store.py
@@ -0,0 +1,29 @@
+import random
+import requests
+
+SITE = 'www.homedepot.com'
+
+agents = [
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36'
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    ]
+
+def get_price(url):
+    headers = {
+        'User-Agent': random.choice(agents), 
+        'authority': 'www.homedepot.com',
+    }
+    r = requests.get(url, headers=headers, timeout=10)
+    text = r.text
+    price_group_start = text.find('"price"')
+    price_group_end = text.find(',', price_group_start)
+    price_value_start = text.find(':', price_group_start)+1
+    price_value = text[price_value_start:price_group_end]
+    return float(price_value)
+
