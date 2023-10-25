419c901a595f34186888fbbafd2e454eb9053e05
builder555 <85308587+builder555@users.noreply.github.com>
2023-10-25 19:20:18 -0400

    put token as a separate variable

--- notifiers/telegram_notifier.py
+++ notifiers/telegram_notifier.py
@@ -6,5 +6,5 @@ async def notify(text:str):
     chat_id = os.getenv('TELEGRAM_CHAT_ID')
     if not token or not chat_id:
         raise Exception('Must specify TELEGRAM_TOKEN and TELEGRAM_CHAT_ID')
-    bot = Bot(token=os.getenv('TELEGRAM_TOKEN'))
+    bot = Bot(token=token)
     await bot.send_message(chat_id=chat_id, text=text)
