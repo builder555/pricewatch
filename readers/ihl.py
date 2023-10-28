11d3ce78cb605f70bb3cd3599b2a9539b6b49c78
builder555 <85308587+builder555@users.noreply.github.com>
2023-10-27 22:14:04 -0400

    add missing argument

--- readers/ihl.py
+++ readers/ihl.py
@@ -2,6 +2,8 @@ import random
 import requests
 from bs4 import BeautifulSoup
 
+SITE = 'www.ihlcanada.com'
+
 agents = [
         'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36'
