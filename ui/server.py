9b8ef98fa085fd2b8bbcb8dbb7c3021cef803146
builder555 <85308587+builder555@users.noreply.github.com>
2023-12-06 10:56:53 -0500

    can delete items

--- ui/server.py
+++ ui/server.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.parse
-
+import json
 class SimpleWebServer(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
@@ -10,13 +10,14 @@ class SimpleWebServer(BaseHTTPRequestHandler):
                 path = '/../items.json'
             else:
                 path = self.path
-            file_to_open = open(path[1:]).read()
+            with open(path[1:], 'r') as file_to_open:
+                data_to_send = file_to_open.read()
             self.send_response(200)
         except FileNotFoundError:
-            file_to_open = "File not found!"
+            data_to_send = "File not found!"
             self.send_response(404)
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.wfile.write(bytes(data_to_send, 'utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -28,6 +29,30 @@ class SimpleWebServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         response = bytes("Post request processed", 'utf-8')
+        self.wfile.write(response)
+    
+    def do_DELETE(self):
+        print(self.path)
+        item_index = int(self.path.split('/')[-1])
+        
+        with open('../items.json', 'r') as f:
+            items = json.load(f)
+
+        if len(items) <= item_index:
+            self.send_response(404)
+            self.end_headers()
+            response = bytes("Item not found", 'utf-8')
+            self.wfile.write(response)
+            return
+        
+        del items[item_index]
+        
+        with open('../items.json', 'w') as f:
+            json.dump(items, f)
+        
+        self.send_response(200)
+        self.end_headers()
+        response = bytes("Delete request processed", 'utf-8')
         self.wfile.write(response)
 
 def run(server_class=HTTPServer, handler_class=SimpleWebServer, port=8700):
