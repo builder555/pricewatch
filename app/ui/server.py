69a102dfdef90b19f220870cd4eaa8b653600f64
builder555 <85308587+builder555@users.noreply.github.com>
2023-12-07 08:00:12 -0500

    clean up and refactor

--- app/ui/server.py
+++ app/ui/server.py
@@ -1,37 +1,36 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.parse
 import json
 import os
-from app.db import add_item, get_items, delete_item, ItemJSONEncoder, Item, save_items
+from app.db import add_item, get_items, delete_item, ItemJSONEncoder, Item, update_item
 from app.readers import get_item_price_with_retries
+from typing import Union
 
 def fetch_price_and_add_item(name: str, url: str):
-    items = get_items()
     price = get_item_price_with_retries(url)
-    items.append(Item(name, url, price))
-    save_items(items)
+    add_item(Item(name, url, price))
 
 class SimpleWebServer(BaseHTTPRequestHandler):
+
+
+    def __send_response(self, response_code: int, response: Union[str, bytes]):
+        self.send_response(200)
+        self.end_headers()
+        if isinstance(response, str):
+            response = response.encode('utf-8')
+        self.wfile.write(response)
+
     def do_GET(self):
         try:
-            base_path = os.path.dirname(__file__)
-            if self.path == '/':
-                path = '/index.html'
-            else:
-                path = self.path
-            if path == '/items':
+            path = self.path.strip('/')
+            if path == 'items':
                 data_to_send = json.dumps(get_items(), cls=ItemJSONEncoder)
-            else:
-                with open(os.path.join(base_path,path[1:]), 'rb') as file_to_open:
-                    data_to_send = file_to_open.read()
-            self.send_response(200)
+                self.__send_response(200, data_to_send)
+                return
+            file_path = os.path.join(os.path.dirname(__file__), path or 'index.html')
+            with open(file_path, 'rb') as fp:
+                self.__send_response(200, fp.read())
         except FileNotFoundError:
-            data_to_send = "File not found!"
-            self.send_response(404)
-        self.end_headers()
-        if isinstance(data_to_send, str):
-            data_to_send = data_to_send.encode('utf-8')
-        self.wfile.write(data_to_send)
+            self.__send_response(404, "File not found!")
     
     def do_PUT(self):
         content_length = int(self.headers['Content-Length'])
@@ -41,14 +40,12 @@ class SimpleWebServer(BaseHTTPRequestHandler):
         
         item_index = int(self.path.split('/')[-1])
         items = get_items()
-        items[item_index].name = data['name']
-        items[item_index].url = data['url']
-        save_items(items)
+        item = items[item_index]
+        item.name = data['name']
+        item.url = data['url']
+        update_item(item_index, item)
         
-        self.send_response(200)
-        self.end_headers()
-        response = bytes("Put request processed", 'utf-8')
-        self.wfile.write(response)
+        self.__send_response(200, "Success")
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -56,18 +53,12 @@ class SimpleWebServer(BaseHTTPRequestHandler):
         
         data = json.loads(post_data)
         fetch_price_and_add_item(data['name'], data['url'])
-        self.send_response(200)
-        self.end_headers()
-        response = bytes("Post request processed", 'utf-8')
-        self.wfile.write(response)
+        self.__send_response(200, "Success")
     
     def do_DELETE(self):
         item_index = int(self.path.split('/')[-1])
         delete_item(item_index)
-        self.send_response(200)
-        self.end_headers()
-        response = bytes("Delete request processed", 'utf-8')
-        self.wfile.write(response)
+        self.__send_response(200, "Success")
 
 def run(server_class=HTTPServer, handler_class=SimpleWebServer, port=8700):
     server_address = ('', port)
