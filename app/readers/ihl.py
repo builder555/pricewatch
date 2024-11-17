3274a4684aedc046d3b59e20f6c07bbb8c5cf832
builder555 <85308587+builder555@users.noreply.github.com>
2024-11-17 18:49:28 -0500

    fix: allow redirects for IHL

--- app/readers/ihl.py
+++ app/readers/ihl.py
@@ -7,7 +7,7 @@ SITE = "www.ihlcanada.com"
 
 
 def get_price(url: str, client: httpx.Client) -> float:
-    r = client.get(url, headers={"User-Agent": random.choice(agents)}, timeout=10)
+    r = client.get(url, headers={"User-Agent": random.choice(agents)}, timeout=10, follow_redirects=True)
     html = r.text
     soup = BeautifulSoup(html, "html.parser")
     tag = soup.find('meta', property='product:price:amount')
